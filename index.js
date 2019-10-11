e0d34d0ff9caa253ec93a9179f33b658ec5f25f1
Karen Nguyen <knguyen4184@yahoo.com>
2019-10-11 12:28:50 -0600

    Functionality

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-var textBox = document.querySelector(".question-textbox");
+var questionInput = document.querySelector(".question-textbox");
 var answerButton = document.getElementById("answer-button");
-var answerText = document.getElementById("answers");
-var questionTe =  document.getElementById("question");
+var clearButton = document.getElementById("clear-button");
+var eightBall = document.querySelector(".eight-ball");
+var givenAnswer = document.getElementById("answer");
+var form = document.querySelector("form");
+var usersQuestion = document.getElementById("question");
+var answers = ["It is certain.",
+               "It is decidedly so.",
+               "Without a doubt.",
+               "Yes - definitely.",
+               "You may rely on it.",
+               "As I see it, yes.",
+               "Most likely.",
+               "Outlook good.",
+               "Yes.",
+               "Signs point to yes.",
+               "Reply hazy, try again.",
+               "Ask again later.",
+               "Better not tell you now.",
+               "Cannot predict now.",
+               "Concentrate and ask again.",
+               "Don't count on it.",
+               "My reply is no.",
+               "My sources say no.",
+               "Outlook not so good.",
+               "Very doubtful.",
+              ];
 
+answerButton.addEventListener("click", onAnswerButtonClick);
+clearButton.addEventListener("click", clearInputs);
+questionInput.addEventListener("keydown", requireInput);
 
-    //
-    // answerButton.addEventListener("click", function() {
-    //   textBox.innerText = question.value;
+function onAnswerButtonClick() {
+  hideEightBall();
+  showQuestion();
+  showAnswer();
+  styleClearButton()
+}
 
-      function addQuestion(question) {
-        var questionText = document.getElementById("question");
-        questionText.innerText = question;
-      }
+function requireInput() {
+  questionInput.value === "";
+}
 
-      answerButton.addEventListener("click", function() {
-        var textBox = document.querySelector(".question-textbox");
-        addQuestion(textBox.value);
-        textBox.value = "";
+function hideEightBall() {
+  eightBall.classList.add("hidden");
+}
 
-      function response() {
-        var answers = ["It is certain.",
-                       "It is decidedly so.",
-                        "Without a doubt.",
-                        "Yes - definitely.",
-                        "You may rely on it.",
-                        "As I see it, yes.",
-                        "Most likely.",
-                        "Outlook good.",
-                        "Yes.",
-                        "Signs point to yes.",
-                        "Reply hazy, try again.",
-                        "Ask again later.",
-                        "Better not tell you now.",
-                        "Cannot predict now.",
-                        "Concentrate and ask again.",
-                        "Don't count on it.",
-                        "My reply is no.",
-                        "My sources say no.",
-                        "Outlook not so good.",
-                        "Very doubtful."
-                      ];
-              response(answerArr) {
-              for (var i = 0; i < answerArr.length; i++) {
-              this.answes.push(answerArr[i])
-        }
+function showQuestion() {
+   usersQuestion.innerText = (`"${questionInput.value}"`);
+   form.reset();
+}
 
-      document.getElementById("answers").innerText = answers[]
+function showAnswer() {
+   var randomAnswer = answers[Math.floor(Math.random() * words.length)];
+    givenAnswer.innerText = answers.value;
+}
 
+function styleClearButton() {
+  clearButton.style.backgroundColor = "#FF0000";
+}
 
-      });
-
-});
+function clearInputs() {
+  usersQuestion.classList.add("hidden");
+  givenAnswer.classList.add("hidden");
+  eightBall.classList.remove("hidden");
+}
